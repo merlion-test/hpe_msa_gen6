35bce1de1b0d2f7265e955e244a68a60cf9830ea
merlion-test <117036347+merlion-test@users.noreply.github.com>
2022-11-28 23:13:07 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -61,8 +61,8 @@ function scrollTo(to, duration = 700) {
 document.addEventListener("DOMContentLoaded", function () {
     let btn = document.querySelector("#toTop");
     window.addEventListener("scroll", function () {
-        // Если прокрутили дальше 1499px, показываем кнопку
-        if (pageYOffset > 1500) {
+        // Если прокрутили дальше 1599px, показываем кнопку
+        if (pageYOffset > 2500) {
             btn.classList.add("show");
             // Иначе прячем
         } else {
@@ -79,7 +79,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function showButtonUp() {
   let btnUp = document.querySelector('#button');
-  if (window.pageYOffset > 400) {
+  if (window.pageYOffset > 2300) {
     btnUp.style.opacity = '1';
   } else {
     btnUp.style.opacity = '0';
@@ -87,3 +87,42 @@ function showButtonUp() {
 }
 
 window.addEventListener('scroll', showButtonUp);
+
+// Анимация при скролле
+
+const animItems = document.querySelectorAll("._anim-items");
+
+if (animItems.length > 0) {
+    window.addEventListener("scroll", animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if (pageYOffset > animItemOffset - animItemPoint && pageYOffset < animItemOffset + animItemHeight) {
+                animItem.classList.add("_active");
+            } else {
+                if (!animItem.classList.contains("_anim-no-hide")) {
+                    animItem.classList.remove("_active");
+                }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+    }
+
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+}
